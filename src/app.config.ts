b9fc2ebe55bb614cb104a93e30c5d99abdc79afe
robeh0018 <123769181+robeh0018@users.noreply.github.com>
2023-09-09 08:07:27 -1100

    app.config.ts added

---
+++ src/app.config.ts
@@ -0,0 +1,22 @@
+import {provideRouter} from "@angular/router";
+import {appRoutes} from "./app/app.routes";
+import {provideStore} from "@ngrx/store";
+import {appReducer} from "./app/store/app.reducer";
+import {provideHttpClient} from "@angular/common/http";
+import {importProvidersFrom, isDevMode} from "@angular/core";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
+import {provideEffects} from "@ngrx/effects";
+import {AuthEffects} from "./app/auth/store/auth.effects";
+import {TodoEffects} from "./app/todo/store/todo.effects";
+
+
+export const appConfig = {
+  providers: [
+    provideRouter(appRoutes),
+    provideStore(appReducer),
+    provideHttpClient(),
+    importProvidersFrom(),
+    provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()}),
+    provideEffects([AuthEffects, TodoEffects])
+  ]
+};
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,7 @@
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
-import {provideStore} from '@ngrx/store';
-import {provideStoreDevtools} from '@ngrx/store-devtools';
-import {importProvidersFrom, isDevMode} from '@angular/core';
-import {provideRouter} from "@angular/router";
-import {appReducer} from "./app/store/app.reducer";
-import {appRoutes} from "./app/app.routes";
-import {provideEffects} from '@ngrx/effects';
-import {AuthEffects} from "./app/auth/store/auth.effects";
-import {provideHttpClient} from "@angular/common/http";
-import {TodoEffects} from "./app/todo/store/todo.effects";
+import {appConfig} from "./app.config";
 
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        provideRouter(appRoutes),
-        provideStore(appReducer),
-        provideHttpClient(),
-        importProvidersFrom(),
-        provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()}),
-        provideEffects([AuthEffects, TodoEffects])
-    ]
-})
-    .catch(() => console.log);
+bootstrapApplication(AppComponent, appConfig)
+  .catch(() => console.log);
